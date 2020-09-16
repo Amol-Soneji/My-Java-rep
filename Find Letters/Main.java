581539d78bd3789cda1ea03081f56c7c3f649f59
Amol Soneji <eon747@live.com>
2020-09-16 01:19:59 -0400

    Update Main.java
    Completed the code for the Main class of the Find Letters java program.  Commenting of the code is still to be finished.

--- Find Letters/Main.java
+++ Find Letters/Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 /**
 *This is the main class for the Find Letters java program.  In this class the user will be prompted to enter some inputs.  After which the main class will utilize the 
-*countInFile class to search, count, and retreive the number of times in total the provided letters were found.  The main class then displays the result.  
+*countInFile class to search, count, and retreive the number of times in total the provided letters or characters were found.  The main class then displays the result.  
 *
 *<br>
-*The first input will be used to determine how many letters (max 3) to search for in a text file.  
+*The first input will be used to determine how many letters or characters (max 3) to search for in a text file.  
 *
 *The next inputs are the letters to search for.  
 *
@@ -19,3 +19,74 @@ import java.util.Scanner;
 *@author  Amol Soneji
 *@version 1.0,  15 Sept 2020
 */
+
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Scanner userInput = new Scanner(System.in);
+    boolean validInput = false;
+    System.out.println("As stated in the README.md file of the java program, this program counts the number of times a specified group of" 
+                       + System.lineSeparator()
+                       + "letters or characters (max 3) are found in a text file.  ");
+    System.out.println("Enter the number of letters or chacters to search for.  (Max 3)  :  ");
+    int numberOfLetters = 0;
+    while(!validInput)
+    {
+      try
+      {
+        numberOfLetters = Integer.parseInt(userInput.nextLine());
+        if((numberOfLetters < 1) || (numberOfLetters > 3))
+          System.out.println("Invalid input.  Please try again.  :  ");
+        else
+          validInput = true;
+      }
+      catch(Exception someException)
+      {
+        System.out.println("Invalid input.  Please try again.  :  ");
+      }
+    }
+    Character[] charsToSearch = new Character[numberOfLetters];
+    System.out.println("Please enter the first letter or character that is to be searched for.  :  ");
+    charsToSearch[0] = (userInput.nextLine()).charAt(0);
+    for(int i = 1; i < numberOfLetters; i++)
+    {
+      validInput = false;
+      System.out.println("Please enter the next letter or character that is to be searched for.  :  ");
+      Character letter;
+      while(!validInput)
+      {
+        letter = (userInput.nextLine()).charAt(0);
+        for(int j = 0; j < i; j++)//Check to see if the same letter or character is already being searched for.  
+        {
+          if(charsToSearch[j] == letter)
+            System.out.println("That letter or character has allready been entered to be searched for.  Please enter a different letter or character.  :  ");
+          else if((charsToSearch[j] != letter) && (j == (i - 1)))
+            validInput = true;
+          else
+            //Do nothing.  
+        }
+      }
+      charsToSearch[i] = letter;
+    }
+    System.out.println("Now that you have entered the letters, it is time to enter the file to be looked at in the search.  "
+                       + System.lineSeparator()
+                       + "If the file that you want to search is located in the same directory as the Java class FilePath for this program, just enter the name of the file.  "
+                       + System.lineSeparator()
+                       + "Otherwise enter the full directory path of the file you want to use.  :  ");
+    String fileName = userInput.nextLine();
+    
+    //
+    countInFile.setFields(fileName, charsToSearch);
+    //
+    countInFile.doCount();
+    //
+    int result = countInFile.getNumbInstances();
+    
+    //
+    System.out.println("The total number of times any one of the " + numberOfLetters + " appeared is " + result + ".  ");
+    System.out.println("Thank you for running this program.  ");
+    System.exit(0);
+  }
+}
