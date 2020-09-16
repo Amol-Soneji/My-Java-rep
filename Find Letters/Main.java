98e03defc20c017b31459cc48aa7cb31f9f53e19
Amol Soneji <eon747@live.com>
2020-09-15 23:47:27 -0400

    Create Main.java
    Created the main class java file.  The java file is still being completed.

---
+++ Find Letters/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+*This is the main class for the Find Letters java program.  In this class the user will be prompted to enter some inputs.  After which the main class will utilize the 
+*countInFile class to search, count, and retreive the number of times in total the provided letters were found.  The main class then displays the result.  
+*
+*<br>
+*The first input will be used to determine how many letters (max 3) to search for in a text file.  
+*
+*The next inputs are the letters to search for.  
+*
+*Afterwards the next input is where the user enters the name of the file, if the compiles java class files are located in the same directory as the file that is to
+*be used.  Or, the user will enter the full directory path to the file to be used.  
+*
+*<br>
+*The program will then utilize and get the results from the countInFile class, and display the results.  
+*<br>
+*
+*@author  Amol Soneji
+*@version 1.0,  15 Sept 2020
+*/
