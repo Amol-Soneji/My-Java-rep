57aa2456f58643fbcac7ef1f66908519895cff34
Amol Soneji <eon747@live.com>
2020-09-22 00:57:29 -0400

    Update convertLengthMeasurement.java
    Added some of the skeletal structures of the methods that will deal with the calculations, as well as the if-elseif-else structure in the usOnlyConvert() method.

--- Unit Converter/convertLengthMeasurement.java
+++ Unit Converter/convertLengthMeasurement.java
@@ -24,5 +24,95 @@ public class convertLengthMeasurement implements measurementTypeConversion
     inputType = inpType;
     outputType = outType;
     input = inp;
+    if(inputType[0] == 0)//Check if US units
+    {
+      if(outputType[0] == 0)//Check if output is also requested in US units.
+      {
+        if(inputType[1] == outputType[1])//Same US unit measurements.  The result will always be the same (ex. input in inches and output in inches as well).  
+        {
+          output = input;
+          return output;
+        }
+        else
+        {
+          usOnlyConvert();
+          return output;
+        }
+      }
+      else//Output is being requested in SI units.  
+      {
+        usToSiConvert();
+        return output;
+      }
+    }
+    else//Input is in SI units.  
+    {
+      if(outputType[0] == 0)//Check if output is being requested in US units.  
+      {
+        siToUsConvert();
+        return output;
+      }
+      else//Ouput is being requested in SI units as well.  
+      {
+        if(inputType[1] == outputType[1])//Same SI unit measurements.  The result will always be the same (ex. input in cm and output in cm as well).  
+        {
+          output = input;
+          return output;
+        }
+        else
+        {
+          siOnlyConvert();
+          return output;
+        }
+      }
+    }
+  }
+  
+  /**
+  *The following method is for converting between one US length measurement to another US length measurement.  This method does not return a value as it ustilizes the output field variable.  
+  */
+  private static void usOnlyConvert()
+  {
+    if(inputType[1] == 0)//Inches
+    {
+      
+    }
+    else if(inputType[1] == 1)//Feet
+    {
+      
+    }
+    else if(inputType[1] == 2)//Yards
+    {
+      
+    }
+    else//Miles
+    {
+      
+    }
+  }
+  
+  /**
+  *The following method is for converting from a US length measurement to a SI unit length measurement.  This method does not return a value as it utilizes the output field variable.  
+  */
+  private static void usToSiConvert()
+  {
+    
+  }
+  
+  /**
+  *The following method is for conerting between one SI unit length measurement to another SI unit length measurement.  
+  *This method does not return a value as it utilizes the output field variable.  
+  */
+  private static void siOnlyConvert()
+  {
+    
+  }
+  
+  /**
+  *The following method is for converting from a SI unit length measurement to a US length measurement.  This method does not return a value as it utilizes the output field variable.  
+  */
+  private static void siToUsConvert()
+  {
+    
   }
 }
