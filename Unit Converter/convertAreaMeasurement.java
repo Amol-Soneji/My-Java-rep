ec56c92da56bf0f326345cd0c5f90e2dd3b23984
Amol Soneji <eon747@live.com>
2020-09-29 04:03:56 -0400

    Create convertAreaMeasurement.java
    Created the class that will deal with area conversions, and the field variables.

---
+++ Unit Converter/convertAreaMeasurement.java
@@ -0,0 +1,18 @@
+/**
+*This class converts measurements of area, from one area measurement to another area measurement.  This class can also convert from US customary untis to SI units, and vice versa as well.  
+*This class also implements the measurementTypeConversion interface.  
+*<br>
+*
+*@author  Amol Soneji
+*@version 1.0, 29 Sept 2020
+*/
+
+
+
+public class convertAreaMeasurement implements measurementTypeConversion
+{
+  private static byte[] inputType;
+  private static byte[] outputType;
+  private static double input;
+  private static double output;
+}
