a79d52659d270835a309fffe21365c6939e9c379
Amol Soneji <eon747@live.com>
2020-09-30 21:47:42 -0400

    Update convertAreaMeasurement.java
    Completed the measurementConvert method for convertAreaMeasurement, and also added incomplete skeletal methods that will be used for calculating the conversion.

--- Unit Converter/convertAreaMeasurement.java
+++ Unit Converter/convertAreaMeasurement.java
@@ -15,4 +15,89 @@ public class convertAreaMeasurement implements measurementTypeConversion
   private static byte[] outputType;
   private static double input;
   private static double output;
+  
+  /**
+  *This method takes in the parameters and automatically calls the methods for calculating the conversion.  It then returns the double data type value represented by output.  
+  */
+  public static double measurementConvert(byte[] inpType, byte[] outType, double inp)
+  {
+    inputType = inpType;
+    outputType = outType;
+    input = inp;
+    if(inputType[0] == 0)//Check if US units
+    {
+      if(outputType[0] == 0)//Check if output is also requested in US units.
+      {
+        if(inputType[1] == outputType[1])//Same US unit measurements.  The result will always be the same (ex. input in inches sq and output in inches sq as well).  
+        {
+          output = input;
+          return output;
+        }
+        else
+        {
+          usOnlyConvert();
+          return output;
+        }
+      }
+      else//Output is being requested in SI units.  
+      {
+        usToSiConvert();
+        return output;
+      }
+    }
+    else//Input is in SI units.  
+    {
+      if(outputType[0] == 0)//Check if output is being requested in US units.  
+      {
+        siToUsConvert();
+        return output;
+      }
+      else//Ouput is being requested in SI units as well.  
+      {
+        if(inputType[1] == outputType[1])//Same SI unit measurements.  The result will always be the same (ex. input in cm sq and output in cm sq as well).  
+        {
+          output = input;
+          return output;
+        }
+        else
+        {
+          siOnlyConvert();
+          return output;
+        }
+      }
+    }
+  }
+  
+  /**
+  *The following method is for converting between one US area measurement to another US area measurement.  This method does not return a value as it ustilizes the output field variable.  
+  */
+  private static void usOnlyConvert()
+  {
+    
+  }
+  
+  /**
+  *The following method is for converting from a US area measurement to a SI unit area measurement.  This method does not return a value as it utilizes the output field variable.  
+  */
+  private static void usToSiConvert()
+  {
+    
+  }
+  
+  /**
+  *The following method is for conerting between one SI unit area measurement to another SI unit area measurement.  
+  *This method does not return a value as it utilizes the output field variable.  
+  */
+  private static void siOnlyConvert()
+  {
+    
+  }
+  
+  /**
+  *The following method is for converting from a SI unit area measurement to a US area measurement.  This method does not return a value as it utilizes the output field variable.  
+  */
+  private static void siToUsConvert()
+  {
+    
+  }
 }
