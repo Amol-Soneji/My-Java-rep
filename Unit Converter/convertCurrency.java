0abb27d4f713f938c8493870b6587b7dd8d08a31
Amol Soneji <eon747@live.com>
2021-01-01 05:43:37 -0500

    Create convertCurrency.java
    Created the convertCurrency class, which will be responsible for converting currency of one country to the currency of another.  Added some field values, with no initialization.

---
+++ Unit Converter/convertCurrency.java
@@ -0,0 +1,19 @@
+/**
+*This class converts currency of one contry, to the currency of another country.  This class utilizes an the getCurrencyInfo class, to get the latest market exchange rate.  
+*<br>
+*
+*@author  Amol Soneji
+*@version 1.0, 01 Jan 2021  ,  Happry New Year!!!
+*/
+
+
+
+public class convertCurrecny
+{
+  private static byte inputCurrency;
+  private static byte outputCurrecy;
+  private static double input;
+  private static double output;
+  
+  
+}
