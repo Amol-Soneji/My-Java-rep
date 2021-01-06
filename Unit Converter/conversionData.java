4e06f68d91b5ba1b8160e47a3e4e551f1cb86b22
Amol Soneji <eon747@live.com>
2021-01-06 04:14:10 -0500

    Create conversionData.java
    Created the class file with the declaration of the class and a comment for what this class is used for.

---
+++ Unit Converter/conversionData.java
@@ -0,0 +1,14 @@
+/**
+*This class gets currency exchange rate data between two countries.  This class is called by the convertCurrncy class.  
+*<br>
+*
+*@author  Amol Soneji
+*@version 1.0, 06 Jan 2021
+*/
+
+
+
+public class conversionData
+{
+  
+}
