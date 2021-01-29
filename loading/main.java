0619fb48591227f443354b3cb34a41f1e643de0c
Amol Soneji <eon747@live.com>
2021-01-29 02:17:29 -0500

    Create main.java
    This class acts as the driver class for the program.  This class was created to make the already finished program, into a usable state.  The loding class was just an example of a Java program.

---
+++ loading/main.java
@@ -0,0 +1,11 @@
+/**This class acts as the driver class for the program.  This class was created to make the already finished program, into a usable state.  The loding class was just an example of
+*a Java program.  
+**/
+
+public class main
+{
+  public static void main(String[] args)
+  {
+    loading.loading();
+  }
+}
