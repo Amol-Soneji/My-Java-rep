c5e66f8c189226aabf4a9d40f884f999735efb42
Amol Soneji <eon747@live.com>
2021-05-04 19:34:35 -0400

    Create UnknownCurrencyException.java
    Created a class that extends the Exception class.  This exception is invoked when an unknown currency is used as the source currency or end currency, in the conversionData class.

---
+++ Unit Converter/UnknownCurrencyException.java
@@ -0,0 +1,37 @@
+/**
+*This class is a subclass of class Exception.  This exception is invoked when an unknown currency has been entered
+*in as either the source currency or the end currency.  Currently in this program, only conversionData could invoke
+*this type of exception.  
+*<br>
+*
+*@author  Amol Soneji
+*@version 1.0, 04 May 2021
+*/
+
+public class UnknownCurrencyException extends Exception
+{
+  public UnknownCurrencyException()
+  {
+    
+  }
+  
+  public UnknownCurrencyException(String message)
+  {
+    super(message);
+  }
+  
+  public UnknownCurrencyException(Throwable cause)
+  {
+    super(cause);
+  }
+  
+  public UnkownCurrencyException(String message, Throwable cause)
+  {
+    super(message, cause);
+  }
+  
+  public UnknownCurrencyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+  {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
