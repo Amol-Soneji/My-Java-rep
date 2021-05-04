26fffcd2485cc8fe5b4777c0ff27e13cc76428e5
Amol Soneji <eon747@live.com>
2021-05-03 20:20:21 -0400

    Update conversionData.java
    Converted two methods into constructors, added the missing class declaration and scope, added some import statements that will be used, and added another method that will be used to get the data of the conversion rate from an external source.  Also renamed a method from getRate to returnRate.

--- Unit Converter/conversionData.java
+++ Unit Converter/conversionData.java
@@ -6,25 +6,61 @@
 *@version 1.0, 06 Jan 2021
 */
 
-private static double manualRate;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-/**
-*The following static class is only called if a manual static conversion rate is to be set.  
-*/
-public static void conversionData(double manRate)
+public class conversionData
 {
-  manualRate = manRate;
-}
+  //Private fields in the class.  
+  private static double rate;
+  private static String srcCurrencyName;
+  private static String endCurrencyName;
+  private static double manualRate;
 
-/**
-*The following class gets the currency echange rate.  
-*/
-public static double getRate(byte inputCurrency, byte outputCurrency)
-{
-  if(inputCurrency == -1)
-    return manualRate;
-  else
+  //Public fields in the class.  
+  public static boolean manual;
+
+  /**
+  *The following constructor is only called if a manual static conversion rate is to be set.  
+  */
+  public conversionData(double manualRate)
+  {
+    conversionData.manualRate = manRate;
+    manual = true;
+  }
+
+  /**
+  *The following constructor is used when trying to get the actual conversion rate value.  
+  */
+  public conversionData(String srcCurrencyName, String endCurrencyName)
+  {
+    conversionData.srcCurrencyName = srcCurrencyName;
+    conversionData.endCurrencyName = endCurrencyName;
+    manual = false;
+  }
+
+  /**
+  *The following method returns the currency echange rate.  
+  */
+  public static double returnRate()
+  {
+    if(manual)
+      return manualRate;
+    else
+    {
+      getData();
+      return rate;
+    }
+  }
+  
+  /**
+  *The following class gets the currency rate data.  
+  */
+  private static void getData()
   {
     
   }
+    
 }
