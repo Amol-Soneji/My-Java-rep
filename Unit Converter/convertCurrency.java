04c0992ab8c112333126317e41d5c634e9e36c59
Amol Soneji <eon747@live.com>
2021-05-13 21:06:03 -0400

    Update convertCurrency.java
    Fixed class name declaration misspelling, and added some more variables and methods.  These were added to make use of the manual conversion rate functionality.

--- Unit Converter/convertCurrency.java
+++ Unit Converter/convertCurrency.java
@@ -10,24 +10,40 @@ import java.util.Scanner;
 
 
 
-public class convertCurrecny
+public class convertCurrency
 {
   private static byte inputCurrency;
   private static byte outputCurrency;
   private static double input;
   private static double rate;
+  private static bool manual;
   
   /**
   *This method takes in the parameters and calls methods required to carry out the calculation of conversion.  It then returns the double data type value represented by output.  
   */
   public static double convertCurrency(byte inpCurrency, byte outCurrency, double inp)
   {
+    manual = false;
     inputCurrency = inpCurrency;
     outputCurrency = outCurrency;
     input = inp;
     if((inputCurrency == outputCurrency) || (input == 0))
       return input;
     getRate();
+    return calculateConversion();
+  }
+  
+  /**
+  *This method is similar to the above mentioned method, except it is used when a fixed pre-defined exchange rate is previously entered in.  
+  */
+  public static double convertCurrency(byte inpCurrency, byte outCurrency, double inp, double rate)
+  {
+    manual = true;
+    inputCurrency = inpCurrency;
+    outputCurrency = outCurrency;
+    input = inp;
+    if((inputCurrency == outputCurrency) || (input == 0))
+      return input;
     return calculateConversion();
   }
   
@@ -44,5 +60,15 @@ public class convertCurrecny
   */
   private static double calculateConversion()
   {
-    return (input / rate);
+    try
+    {
+      if((rate <= 0) && (manual))
+        throw InvalidConversionRateException
+      return (input / rate);
+    }
+    catch(InvalidConversionRateException e)
+    {
+      System.out.println("Invalid conversion rate entered.  Please try again from begining of conversion process.  ");
+      return 0.00;
+    }
   }
