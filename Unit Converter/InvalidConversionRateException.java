e25dd0ba37ff33d54d16228d009c115692bcaabc
Amol Soneji <eon747@live.com>
2021-05-19 23:50:51 -0400

    Create InvalidConversionRateException.java
    Created an Exception that is called when a manual conversion rate is entered that is equal to or less than 0.

---
+++ Unit Converter/InvalidConversionRateException.java
@@ -0,0 +1,37 @@
+/**
+*This class is a subclass of class Exception.  This exception is invoked when a manual conversion rate has been entered
+*in as a value that is equal to or less than 0.  Currently in this program, only conversionData could invoke
+*this type of exception.  
+*<br>
+*
+*@author  Amol Soneji
+*@version 1.0, 04 May 2021
+*/
+
+public class InvalidConversionRateException extends Exception
+{
+  public InvalidConversionRateException()
+  {
+    //Empty constructor.  
+  }
+  
+  public InvalidConversionRateException(String message)
+  {
+    super(message);
+  }
+  
+  public InvalidConversionRateException(Throwable cause)
+  {
+    super(cause);
+  }
+  
+  public InvalidConversionRateException(String message, Throwable cause)
+  {
+    super(message, cause);
+  }
+  
+  public InvalidConversionRateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+  {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
