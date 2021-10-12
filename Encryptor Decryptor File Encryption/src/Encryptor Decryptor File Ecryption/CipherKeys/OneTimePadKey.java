52759a26b0af6bcb23e49df8e5c8f358815aa12e
amol soneji <eon747@live.com>
2021-10-12 02:25:22 -0400

    Made some updates to some UML diagram components.

---
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeys/OneTimePadKey.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package CipherKeys;
+
+/**
+ * @author Amol Soneji
+ *
+ */
+public class OneTimePadKey 
+{
+	private int textLength;
+	private String key;
+	
+	public OneTimePadKey()
+	{
+		createKey();
+	}
+	
+}
