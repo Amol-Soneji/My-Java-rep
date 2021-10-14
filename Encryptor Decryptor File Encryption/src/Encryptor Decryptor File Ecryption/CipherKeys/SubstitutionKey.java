cb6a2eb5cd939678cd774cfd4d991774abbee3e7
amol soneji <eon747@live.com>
2021-10-14 19:11:27 -0400

    Continued adding code to SubstitutionKey class.

--- Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeys/SubstitutionKey.java
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeys/SubstitutionKey.java
@@ -40,10 +40,22 @@ public class SubstitutionKey
 	{
 		SecureRandom random = new SecureRandom();
 		if(booleanMode) {
+			int randomInteger = -1;
+			while((randomInteger > 1112064) && (randomInteger < 2) && (gcd(randomInteger, 1112064) != 1)) {
+				randomInteger = random.nextInt(2097152);
+			}
+			key = randomInteger;
 			
 		}
 		else {
 			key = random.nextInt();
 		}
+	}
+	
+	private int gcd(int a, int b)
+	{
+		if(b == 0)
+			return a;
+		return gcd(b, a % b);
 	}
 }
