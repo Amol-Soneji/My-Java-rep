8712a9a095b98e02304373dd8f91b92766b944bd
amol soneji <eon747@live.com>
2021-10-15 02:12:18 -0400

    Made updates to some UML diagaram components, and completed the SubstitutionKey Java class.

---
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeys/KeyCreationException.java
@@ -0,0 +1,30 @@
+package CipherKeys;
+
+public class KeyCreationException extends Exception {
+
+	public KeyCreationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public KeyCreationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public KeyCreationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public KeyCreationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public KeyCreationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeys/SubstitutionKey.java
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeys/SubstitutionKey.java
@@ -1,9 +1,8 @@
-/**
- * 
- */
+/**/
 package CipherKeys;
 
 import java.security.SecureRandom;
+import java.math.BigInteger;
 
 /**
  * @author eon74
@@ -16,7 +15,7 @@ public class SubstitutionKey
 	private int arbitraryB;
 	private boolean booleanMode;
 	
-	public SubstitutionKey(boolean booleanMode)
+	public SubstitutionKey(boolean booleanMode) throws KeyCreationException
 	{
 		this.booleanMode = booleanMode;
 		createKey();
@@ -36,20 +35,48 @@ public class SubstitutionKey
 		booleanMode = true;
 	}
 	
-	private void createKey()
+	private void createKey() throws KeyCreationException
 	{
 		SecureRandom random = new SecureRandom();
 		if(booleanMode) {
 			int randomInteger = -1;
-			while((randomInteger > 1112064) && (randomInteger < 2) && (gcd(randomInteger, 1112064) != 1)) {
+			while((randomInteger > 1112064) || (randomInteger < 2) || (gcd(randomInteger, 1112064) != 1)) {
 				randomInteger = random.nextInt(2097152);
 			}
 			key = randomInteger;
-			
+			try
+			{
+				affineDecKey = (BigInteger.valueOf(key).modInverse(BigInteger.valueOf(1112064))).intValue();
+			}
+			catch(ArithmeticException e)
+			{
+				System.out.println("Internal program error.  ");
+				throw new KeyCreationException();
+			}
+			randomInteger = -1;
+			while((randomInteger > 1112064) || (randomInteger < 2) || (randomInteger == key) || (randomInteger == affineDecKey)) {
+				randomInteger = random.nextInt(2097152);
+			}
+			arbitraryB = randomInteger;
 		}
 		else {
 			key = random.nextInt();
 		}
+	}
+	
+	public int getKeyVal()
+	{
+		return key;
+	}
+	
+	public int getAffineDecKey()
+	{
+		return affineDecKey;
+	}
+	
+	public int getArbitraryB()
+	{
+		return arbitraryB;
 	}
 	
 	private int gcd(int a, int b)
