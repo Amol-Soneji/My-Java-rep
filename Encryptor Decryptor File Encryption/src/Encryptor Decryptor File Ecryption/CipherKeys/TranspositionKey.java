460ad618011c70e8eb38299dba2fe61a096d24f3
amol soneji <eon747@live.com>
2021-10-17 23:36:43 -0400

    Created and am currently coding the TranspositionKey Java class file.

---
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeys/TranspositionKey.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package CipherKeys;
+
+/**
+ * @author Amol Soneji
+ *
+ */
+public class TranspositionKey {
+
+	private int key;
+	private boolean usePunct;
+	
+	/**
+	 * 
+	 */
+	public TranspositionKey() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
