f2f9c4f067d204573a163dfe379f6b46c707fda4
amol soneji <eon747@live.com>
2021-10-20 21:46:49 -0400

    Removed planed feture of having multiple bit length keys available in first version.  I will start with 128-bit and once program completed, work up from there.

---
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeys/BlockKey.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package CipherKeys;
+
+/**
+ * @author eon74
+ *
+ */
+public class BlockKey {
+
+	/**
+	 * 
+	 */
+	public BlockKey() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
