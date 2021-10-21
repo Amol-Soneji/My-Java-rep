d6d7ac98c59fc892d87c406d0f3001c02f5b846e
amol soneji <eon747@live.com>
2021-10-20 23:02:04 -0400

    Updated a UML Diagram component and KeyPackage UML package compnent images.  Boolean value for enDecryptionMethod for two of the three contructors in the BlockKey class do not not need to be passed, as it can implicily be determed what the value should be.

--- Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeys/BlockKey.java
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeys/BlockKey.java
@@ -3,17 +3,45 @@
  */
 package CipherKeys;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.SecureRandom;
+
 /**
- * @author eon74
+ * @author Amol Soneji
  *
  */
 public class BlockKey {
 
+	private SecretKey key;
+	private byte[] IV;
+	private int authenticationTagLength;
+	private boolean enDecryptionMethod;
+	
 	/**
 	 * 
 	 */
-	public BlockKey() {
-		// TODO Auto-generated constructor stub
+	public BlockKey(boolean enDecryptionMethod) 
+	{
+		this.enDecryptionMethod = enDecryptionMethod;
+		createKey();
 	}
+	
+	public BlockKey(SecretKey key, byte[] IV, int authenticataionTagLength)
+	{
+		this.key = key;
+		this.IV = IV;
+		this.authenticationTagLength = authenticationTagLength;
+		enDecryptionMethod = true; //True means GCM mode.  
+	}
+	
+	public BlockKey(SecretKey key, byte[] IV)
+	{
+		this.key = key;
+		this.IV = IV;
+		enDecryptionMethod = false; //False means CBC mode.  
+	}
+	
+	
 
 }
