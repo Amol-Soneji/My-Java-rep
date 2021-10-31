444c2ef4261f60181a8277389ede8fccf4c6095f
amol soneji <eon747@live.com>
2021-10-31 01:40:19 -0400

    upd

--- Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherPackage/RailFenceCipher.java
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherPackage/RailFenceCipher.java
@@ -3,45 +3,86 @@
  */
 package CipherPackage;
 
+import java.util.stream.IntStream;
+import CipherKeys.TranspositionKey;
+
 /**
- * @author eon74
+ * @author Amol Soneji
  *
  */
-public class RailFenceCipher extends TranspositionCipher {
+public class RailFenceCipher extends TranspositionCipher 
+{
+	
+	private String[] cipherTextBuilder;
+	private String[] plainTextBuilder;
+	private String plainText;
+	private String cipherText;
+	private boolean mode;
+	private int textLength;
+	private int requiredLengthsCheck;
 
 	/**
 	 * 
 	 */
-	public RailFenceCipher() {
-		// TODO Auto-generated constructor stub
+	public RailFenceCipher(String text, TranspositionKey key, boolean mode) 
+	{
+		this.mode = mode;
+		if(mode) { // True for encrypt.  
+			super.setKey(key);
+			if(super.getPunct())  {  //Keep punctuation in result.  
+				plainText = text;
+				textLength = text.length();
+			}
+			else  {  //Don't keep punctuation in result.  
+				IntStream textInts = text.codePoints();
+				int[] intTextArray = textInts.toArray();
+				for(int index = 0; index < text.length(); index++) {
+					if(Character.isLetter(intTextArray[index])) // Check if not punctuation.  
+						plainText = plainText + String.copyValueOf(Character.toChars(intTextArray[index]));
+				}
+				textLength = plainText.length();
+			}
+			cipherTextBuilder = new String[key.getKey()];
+		}
+		else {  //Decrypt
+			cipherText = text;
+			super.setKey(key);
+			textLength = text.length();
+			plainTextBuilder = new String[key.getKey()];
+		}
 	}
 
 	@Override
-	public String compute(boolean resultType) {
+	public String compute(boolean resultType)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected String encrypt() {
+	protected String encrypt() 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected String encryptCharCodes() {
+	protected String encryptCharCodes() 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected String decrypt() {
+	protected String decrypt() 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected String decryptCharCodes() {
+	protected String decryptCharCodes() 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
