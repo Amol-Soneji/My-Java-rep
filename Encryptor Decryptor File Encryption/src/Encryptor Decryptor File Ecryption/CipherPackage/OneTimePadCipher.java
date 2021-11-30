07a74f5d6d3ced389ed68889cf33b1dee38f739e
amol soneji <eon747@live.com>
2021-11-29 23:52:42 -0500

    Updated the OneTimePadCipher diagram to remove unnessessary methods that I now think do not need to be implmented.  Also completed the code for the first version of OneTimePadCipher Java class.

--- Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherPackage/OneTimePadCipher.java
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherPackage/OneTimePadCipher.java
@@ -3,6 +3,8 @@
  */
 package CipherPackage;
 
+import java.io.UnsupportedEncodingException;
+
 import CipherKeys.OneTimePadKey;
 
 /**
@@ -12,8 +14,8 @@ import CipherKeys.OneTimePadKey;
 public class OneTimePadCipher 
 {
 	
-	private String cipherTextBuilder;
-	private String plainTextBuilder;
+	private byte[] cipherBytes;
+	private byte[] plainBytes;
 	private String cipherText;
 	private String plainText;
 	private boolean mode;
@@ -43,10 +45,104 @@ public class OneTimePadCipher
 	{
 		if(mode)
 		{
-			if(inputType) // Is not a string of bits.  
+			if(inputType) // Is not a String of bytes, is a literal plaintext.  
 			{
-				cipherText = 
+				try 
+				{
+					plainBytes = plainText.getBytes("UTF-16");
+				} 
+				catch (UnsupportedEncodingException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					System.out.println("Error could not interpret text.  ");
+					System.out.println("Fatal program error, programm will close in 10 seconds.  ");
+					for(int i = 0; i < 100000; i++)
+					{
+						i = i; // Do nothing.  
+					}
+					System.exit(1);
+				}
 			}
+			else // Is a String of bytes, where each byte is separated by a space.  
+			{
+				String[] stringBytes = plainText.split(" ");
+				for(int index = 0; index < stringBytes.length; index++)
+				{
+					plainBytes[index] = Byte.parseByte(stringBytes[index]);
+				}
+			}
+			return encrypt();
 		}
+		else
+		{
+			try
+			{
+				cipherBytes = cipherText.getBytes("UTF-16");
+			}
+			catch(UnsupportedEncodingException e)
+			{
+				e.printStackTrace();
+				System.out.println("Error could not interpret text.  ");
+				System.out.println("Fatal program error, programm will close in 10 seconds.  ");
+				for(int i = 0; i < 100000; i++)
+				{
+					i = i; // Do nothing.  
+				}
+				System.exit(1);
+			}
+			return decrypt();
+		}
+	}
+	
+	protected String encrypt()
+	{
+		cipherBytes = new byte[key.getTextLength()];
+		for(int index = 0; index < plainBytes.length; index++)
+		{
+			cipherBytes[index] = (byte)(key.getKey()[index] ^ plainBytes[index]);
+		}
+		try 
+		{
+			cipherText = new String(cipherBytes, "UTF-16");
+		} 
+		catch (UnsupportedEncodingException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Error could not interpret text.  ");
+			System.out.println("Fatal program error, programm will close in 10 seconds.  ");
+			for(int i = 0; i < 100000; i++)
+			{
+				i = i; // Do nothing.  
+			}
+			System.exit(1);
+		}
+		return cipherText;
+	}
+	
+	protected String decrypt()
+	{
+		plainBytes = new byte[key.getTextLength()];
+		for(int index = 0; index < cipherBytes.length; index++)
+		{
+			plainBytes[index] = (byte)(key.getKey()[index] ^ cipherBytes[index]);
+		}
+		try
+		{
+			plainText = new String(cipherBytes, "UTF-16");
+		}
+		catch(UnsupportedEncodingException e)
+		{
+			e.printStackTrace();
+			System.out.println("Error could not interpret text.  ");
+			System.out.println("Fatal program error, programm will close in 10 seconds.  ");
+			for(int i = 0; i < 100000; i++)
+			{
+				i = i; // Do nothing.  
+			}
+			System.exit(1);
+		}
+		return plainText;
 	}
 }
