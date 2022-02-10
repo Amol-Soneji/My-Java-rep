264ba378267f7b04f7ad1ed9e3c081c01aee031e
Amol-Soneji <eon747@live.com>
2022-02-10 15:42:27 -0500

    Added some code to main, as well as finished the method skeletal structure for main.  Will update the UML class components diagram for the main class in next commit, to include a small change.

--- Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/main/main.java
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/main/main.java
@@ -1,5 +1,38 @@
 
 package main;
 
+import CipherKeys.*;
+import CipherPackage.*;
 import java.util.Scanner;
 
+public class main
+{
+	private static final String VERSION_NUMBER = "0.9";
+	
+	public static void main(String[] args)
+	{
+		Scanner userInput = new Scanner(System.in());
+		System.out.println("Encryptor Decryptor File Encryption, Version : " + VERSION_NUMBER);
+	}
+	
+	private static int askAction(int availOptionRefferer)
+	{
+		
+	}
+	
+	private static boolean encryptDecrypt(int option, int keyType)
+	{
+		
+	}
+	
+	private static String readFile(String fileName)
+	{
+		
+	}
+	
+	private static boolean writeFile(String fileName, String contents)
+	{
+		
+
+	}
+}
