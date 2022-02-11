2da467ff5f01589f6ef7f557d44425db45c796c3
Amol Soneji <eon747@live.com>
2022-02-11 00:24:06 -0500

    Updated some UML diagrams to reflect some changes.

--- Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeyStorage.java
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/CipherKeyStorage.java
@@ -29,6 +29,7 @@ public class CipherKeyStorage
 		try 
 		{
 			this.conn = DriverManager.getConnection("jdbc:sqlite:" + FILE_PATH);
+			checkDBStats();
 		} 
 		catch (SQLException e) 
 		{
@@ -48,91 +49,8 @@ public class CipherKeyStorage
 		this.dbName = dbName;
 		try
 		{
-			boolean[] tblsExistant = new boolean[7];
 			this.conn = DriverManager.getConnection("jdbc:sqlite:" + dbName);
-			Statement stmt = conn.createStatement();
-			ResultSet rSet = stmt.executeQuery("SHOW TABLES"); //Check to see if any of the tables needed 
-			//by this program are not in the database yet.  
-			while(rSet.next())
-			{
-				String tblName = rSet.getString(1);
-				if(tblName.equalsIgnoreCase("AES-GCM"))
-					tblsExistant[0] = true;
-				else if(tblName.equalsIgnoreCase("AES-CBC"))
-					tblsExistant[1] = true;
-				else if(tblName.equalsIgnoreCase("Affine"))
-					tblsExistant[2] = true;
-				else if(tblName.equalsIgnoreCase("Caeser"))
-					tblsExistant[3] = true;
-				else if(tblName.equalsIgnoreCase("OneTimePad"))
-					tblsExistant[4] = true;
-				else if(tblName.equalsIgnoreCase("RailFence"))
-					tblsExistant[5] = true;
-				else if(tblName.equalsIgnoreCase("Vigenere"))
-					tblsExistant[6] = true;
-				else
-					tblName = tblName; //Do nothing, user defined table.  
-			}
-			//Now create any of the missing tables if needed.  
-			if(!(tblsExistant[0]))
-			{
-				stmt.executeUpdate("CREATE TABLE AES-GCM ("
-								   + "document_name varchar(255) NOT NULL, "
-								   + "key BLOB NOT NULL, "
-								   + "iv BLOB NOT NULL, "
-								   + "auth_tag_len integer NOT NULL, "
-								   + "PRIMARY KEY (document_name))");
-			}
-			if(!(tblsExistant[1]))
-			{
-				stmt.executeUpdate("CREATE TABLE AES-CBC ("
-								   + "document_name varchar(255) NOT NULL, "
-								   + "key BLOB NOT NULL, "
-								   + "iv BLOB NOT NULL, "
-								   + "PRIMARY KEY (document_name))");
-			}
-			if(!(tblsExistant[2]))
-			{
-				stmt.executeUpdate("CREATE TABLE Affine ("
-								   + "document_name varchar(255) NOT NULL, "
-								   + "key integer NOT NULL, "
-								   + "affine_dec_key integer NOT NULL, "
-								   + "arbitrary_x integer NOT NULL, "
-								   + "PRIMARY KEY (document_name))");
-			}
-			if(!(tblsExistant[3]))
-			{
-				stmt.executeUpdate("CREATE TABLE Caeser ("
-								   + "document_name varchar(255) NOT NULL, "
-								   + "key integer NOT NULL, "
-								   + "PRIMARY KEY (document_name))");
-			}
-			if(!(tblsExistant[4]))
-			{
-				stmt.executeUpdate("CREATE TABLE OneTimePad ("
-								   + "document_name varchar(255) NOT NULL, "
-								   + "text_len integer NOT NULL, "
-								   + "key BLOB NOT NULL, "
-								   + "PRIMARY KEY (document_name))");
-			}
-			if(!(tblsExistant[5]))
-			{
-				stmt.executeUpdate("CREATE TABLE RailFence ("
-								   + "document_name varchar(255) NOT NULL, "
-								   + "key integer NOT NULL, "
-								   + "use_punct BLOB NOT NULL, "
-								   + "PRIMARY KEY (document_name))");
-			}
-			if(!(tblsExistant[6]))
-			{
-				stmt.executeUpdate("CREATE TABLE Vigenere ("
-								   + "document_name varchar(255) NOT NULL, "
-								   + "key varchar(255) NOT NULL, "
-								   + "use_punct BLOB NOT NULL, "
-								   + "PRIMARY KEY (document_name))");
-			}
-			rSet.close();
-			stmt.close();
+			checkDBStats();
 		}
 		catch(SQLException e)
 		{
@@ -551,6 +469,117 @@ public class CipherKeyStorage
 			e.printStackTrace();
 			System.out.println("There was an error closing the database.  \n"
 							   + "Program will close in 10 seconds.  ");
+			for(int i = 0; i < 10000; i++)
+			{
+				i = i; //Do nothing.  
+			}
+			System.exit(1);
+		}
+	}
+	
+	private void checkDBStats()
+	{
+		try
+		{
+			boolean[] tblsExistant = new boolean[7];
+			Statement stmt = conn.createStatement();
+			ResultSet rSet = stmt.executeQuery("SHOW TABLES"); //Check to see if any of the tables needed 
+			//by this program are not in the database yet.  
+			while(rSet.next())
+			{
+				String tblName = rSet.getString(1);
+				if(tblName.equalsIgnoreCase("AES-GCM"))
+					tblsExistant[0] = true;
+				else if(tblName.equalsIgnoreCase("AES-CBC"))
+					tblsExistant[1] = true;
+				else if(tblName.equalsIgnoreCase("Affine"))
+					tblsExistant[2] = true;
+				else if(tblName.equalsIgnoreCase("Caeser"))
+					tblsExistant[3] = true;
+				else if(tblName.equalsIgnoreCase("OneTimePad"))
+					tblsExistant[4] = true;
+				else if(tblName.equalsIgnoreCase("RailFence"))
+					tblsExistant[5] = true;
+				else if(tblName.equalsIgnoreCase("Vigenere"))
+					tblsExistant[6] = true;
+				else
+					tblName = tblName; //Do nothing, user defined table if using user defined db.  
+			}
+			//Now create any of the missing tables if needed.  
+			if(!(tblsExistant[0]))
+			{
+				stmt.executeUpdate("CREATE TABLE AES-GCM ("
+								   + "document_name varchar(255) NOT NULL, "
+								   + "key BLOB NOT NULL, "
+								   + "iv BLOB NOT NULL, "
+								   + "auth_tag_len integer NOT NULL, "
+								   + "PRIMARY KEY (document_name))");
+			}
+			if(!(tblsExistant[1]))
+			{
+				stmt.executeUpdate("CREATE TABLE AES-CBC ("
+								   + "document_name varchar(255) NOT NULL, "
+								   + "key BLOB NOT NULL, "
+								   + "iv BLOB NOT NULL, "
+								   + "PRIMARY KEY (document_name))");
+			}
+			if(!(tblsExistant[2]))
+			{
+				stmt.executeUpdate("CREATE TABLE Affine ("
+								   + "document_name varchar(255) NOT NULL, "
+								   + "key integer NOT NULL, "
+								   + "affine_dec_key integer NOT NULL, "
+								   + "arbitrary_x integer NOT NULL, "
+								   + "PRIMARY KEY (document_name))");
+			}
+			if(!(tblsExistant[3]))
+			{
+				stmt.executeUpdate("CREATE TABLE Caeser ("
+								   + "document_name varchar(255) NOT NULL, "
+								   + "key integer NOT NULL, "
+								   + "PRIMARY KEY (document_name))");
+			}
+			if(!(tblsExistant[4]))
+			{
+				stmt.executeUpdate("CREATE TABLE OneTimePad ("
+								   + "document_name varchar(255) NOT NULL, "
+								   + "text_len integer NOT NULL, "
+								   + "key BLOB NOT NULL, "
+								   + "PRIMARY KEY (document_name))");
+			}
+			if(!(tblsExistant[5]))
+			{
+				stmt.executeUpdate("CREATE TABLE RailFence ("
+								   + "document_name varchar(255) NOT NULL, "
+								   + "key integer NOT NULL, "
+								   + "use_punct BLOB NOT NULL, "
+								   + "PRIMARY KEY (document_name))");
+			}
+			if(!(tblsExistant[6]))
+			{
+				stmt.executeUpdate("CREATE TABLE Vigenere ("
+								   + "document_name varchar(255) NOT NULL, "
+								   + "key varchar(255) NOT NULL, "
+								   + "use_punct BLOB NOT NULL, "
+								   + "PRIMARY KEY (document_name))");
+			}
+			rSet.close();
+			stmt.close();
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			try 
+			{
+				this.conn.close(); //Release resource.  
+			} 
+			catch (SQLException e1) 
+			{
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.out.println("There was an internal error with the program.  \n"
+							  + "The program will exit in 10 seconds.  ");
 			for(int i = 0; i < 10000; i++)
 			{
 				i = i; //Do nothing.  
--- Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/main.java
+++ Encryptor Decryptor File Encryption/src/Encryptor Decryptor File Ecryption/main.java
@@ -1,18 +1,71 @@
-
-
-
 import CipherKeys.*;
 import CipherPackage.*;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class main
 {
 	private static final String VERSION_NUMBER = "0.9";
+	private static CipherKeyStorage dbAccess;
 	
 	public static void main(String[] args)
 	{
-		Scanner userInput = new Scanner(System.in());
+		Scanner userInput = new Scanner(System.in);
 		System.out.println("Encryptor Decryptor File Encryption, Version : " + VERSION_NUMBER);
+		boolean defaultExists = false;
+		while(true)
+		{
+			if(!defaultExists)
+			{
+				try
+				{
+					File dbFile = new File("Keys.db");
+					Scanner fileTest = new Scanner(dbFile);
+					defaultExists = true;
+					fileTest.close();
+				}
+				catch(FileNotFoundException e)
+				{
+					defaultExists = false;
+				}
+			}
+			int input = 0;
+			if(defaultExists)
+			{
+				while((input < 1) || ((input > 2) && (input < 4)) || (input > 4))
+				{
+					System.out.println("Enter 1 to use the deafult database that exists.  Or enter 2 to use your "
+									   + "own database that is compatible with this program (see README.md).  "
+									   + "Enter 5 to exit the program.  :  ");
+					input = userInput.nextInt();
+				}
+			}
+			else
+			{
+				while((input < 3) || (input > 4))
+				{
+					System.out.println("Enter 3 to create a default database to use. Or enter 2 to use your own "
+									   + "database the is compatible with this program (see README.md).  "
+									   + "Enter 4 to exit the program.  :  ");
+					input = userInput.nextInt();
+				}
+			}
+			if(input == 1)
+			{
+				dbAccess = new CipherKeyStorage();
+			}
+			else if(input == 2)
+			{
+				
+			}
+			else if(input == 3)
+			{
+				
+			}
+			else
+				break;
+		}
 	}
 	
 	private static int askAction(int availOptionRefferer)
